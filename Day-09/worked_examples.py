0905532fcc3da95324ce5181c3bf472356880af2
sgpravin-GitHub <60766739+sgpravin-GitHub@users.noreply.github.com>
2024-09-23 07:44:36 +0000

    added worked examples

---
+++ Day-09/worked_examples.py
@@ -0,0 +1,18 @@
+#For loop
+colors=['red','blue','orange','yellow','black']
+
+for x in colors: #using list in loops
+    if (x=='orange'):
+        exit
+    else:
+        print(x)
+
+######printing 5 table
+for i in range(11):
+    if(i==0):
+        exit
+    else:  
+        print("5 * " , i , " = " , (5*i))
+
+
+###
