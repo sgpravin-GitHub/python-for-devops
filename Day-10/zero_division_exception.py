e874e7a42f9aa206f352ad7d0c2ef8d52240f6e4
sgpravin-GitHub <60766739+sgpravin-GitHub@users.noreply.github.com>
2024-09-26 07:58:24 +0000

    added new example

--- Day-10/zero_division_exception.py
+++ Day-10/zero_division_exception.py
@@ -1,6 +1,5 @@
 #This function will show how to use try & except block using ZeroDivisionError
-
-import sys
+""" import sys
 
 print("please provide 2 numbers to find the division of them")
 
@@ -13,4 +12,17 @@ except ZeroDivisionError:
     print("please provide the value greater than ZERO")
     exit()
 
-print(result)
+print(result) """
+import sys
+
+print("enter 2 numbers with spaces between them")
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+try:
+    result = num1/num2
+except ZeroDivisionError:
+    print("we can't divide any number by ZERO")
+    exit()
+
+print("division of these numbers : " , result)
