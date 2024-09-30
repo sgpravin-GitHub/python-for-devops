6a861b7b7b61857018b84af922d9f1b1bec8815a
sgpravin-GitHub <60766739+sgpravin-GitHub@users.noreply.github.com>
2024-09-30 08:27:12 +0000

    updated the code

--- Day-11/git_pull_request.py
+++ Day-11/git_pull_request.py
@@ -1,1 +1,13 @@
 #get pull request information on GIT repository(Kubernetes repository) using python
+
+import requests
+
+response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
+output = response.json()
+# print(type(output)) => list
+for i in range(len(output)): #it give the whole list of users in the json file
+    print(i , " = ", output[i]["user"]["login"])
+
+#to delete a key from dectionary:
+del output[0]["name"]["id"]
+
