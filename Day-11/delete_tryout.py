36d2449cddc62c45d9ae22fcbfe7b3e3450d8130
sgpravin-GitHub <60766739+sgpravin-GitHub@users.noreply.github.com>
2024-09-30 06:43:04 +0000

    practice file added

---
+++ Day-11/delete_tryout.py
@@ -0,0 +1,26 @@
+student_info = [
+    {
+        "student_name" : "xxx",
+        "age" : "16",
+        "class" : "10",
+        "school" : "KLE school"
+    },
+    {
+        "student_name" : "yyy",
+        "age" : "15",
+        "class" : "9",
+        "school" : "KLE school"
+    },
+    {
+        "student_name" : "zzz",
+        "age" : "14",
+        "class" : "8",
+        "school" : "KLE school"
+    },
+    {
+        "student_name" : "AAA",
+        "age" : "13",
+        "class" : "7",
+        "school" : "KLE school"
+    }
+]
